20a5df65f5e87335297541998adbc8d7aa70e6cc
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-02-13 21:22:10 +0530

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+var translator = $('body').translate({
+
+    lang: "English",
+
+    t: local
+
+});
+
+function addActivators() {
+    // Get all div elements
+    var divs = document.querySelectorAll('div.course-item');
+
+    // Loop through each div element
+    divs.forEach(function (div) {
+        // Add click event listener to each div
+        div.addEventListener('click', function () {
+
+            div.childNodes.forEach(node => {
+                if (node.nodeName == "P") {
+
+      
+                    document.getElementById("video title").textContent = node.textContent;
+                    document.getElementById("video title").setAttribute("data-trn-key", node.getAttribute("textContentKey"));
+        
+                    translator.lang(document.getElementById("lang selector").value)
+                    document.getElementById("video player").setAttribute("src", node.textContent + ".mp4");
+                }
+            })
+            // Remove 'active' class from all divs
+            divs.forEach(function (d) {
+                d.classList.remove('active');
+            });
+            // Add 'active' class to the clicked div
+            div.classList.add('active');
+        });
+    });
+}
+addActivators()
+
+function languageHandler(event) {
+    console.log("Language changed to " + event.target.value)
+    translator.lang(event.target.value)
+
+}
+
+function jumpTo(timeInSecs) {
+
+    document.getElementById("video player").currentTime = timeInSecs;
+}
+function jumpHandler(event) {
+    
+    let val = parseInt(event.target.getAttribute("timevalue"))
+    jumpTo(val)
+
+}
+
+let arr = document.getElementsByClassName("timestamp")
+console.log("timestamps:")
+console.log(arr)
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    element.addEventListener("click", jumpHandler)
+    
+}
+ 
---
+++ lang_strings.js
@@ -0,0 +1,56 @@
+let local = {
+
+    Language: {
+        English: "Language",
+        Hindi: "भाषा",
+    },
+
+    vid1: {
+        English: "How to Make Websites with Python",
+        Hindi: "पाइथॉन से वेबसाइट कैसे बनाएं",
+    },
+
+    vid2: {
+        English: "Getting started with React JS",
+        Hindi: "रिएक्ट जेएस के साथ शुरुआत करना",
+    },
+    timestamps: {
+        English: "Timestamps",
+        Hindi: "टाइम स्टाम्प्स",
+        
+    },
+
+    shortnotes: {
+        English: "Short Notes",
+        Hindi : "अल्प टिप्पणियां"
+    },
+
+    lorem: {
+        English: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptate facere, blanditiis repudiandae dignissimos eum ab quas, commodi deserunt voluptates molestiae saepe consectetur incidunt adipisci magni perspiciatis dolore fugiat culpa.",
+        Hindi: "लोरेम इप्सम डोलर सिट अमेट कंसेक्टेचर एडिपिसिसिंग एलीट। ईओएस वॉलुप्टेट फेसरे, ब्लैंडिटिस रिपुडियनडे डिग्निसिमोस ईम एबी क्वास, कमोडी डेसेरंट वॉलुप्टेट्स मोलेस्टिया सेपे कॉन्सेक्टेचर इंसिडंट एडिपिस्की मैग्नी पर्सपिसियाटिस डोलोर फ्यूगिएट कुल्पा।",
+        
+    },
+    ts1: {
+        English: "Installing and set up environment",
+        Hindi: "वातावरण स्थापित करना और स्थापित करना",
+        
+    },
+    ts2: {
+        English: "Running first program",
+        Hindi: "पहला प्रोग्राम चल रहा है",
+        
+    },
+    ts3: {
+        English: "Common errors in programming",
+        Hindi: "प्रोग्रामिंग में सामान्य त्रुटियाँ",
+        
+    },
+    course_mat: {
+        English: "Course material :",
+        Hindi : "पाठ्यक्रम सामग्री :"
+    }
+
+};
+window.local = local;
+
+
